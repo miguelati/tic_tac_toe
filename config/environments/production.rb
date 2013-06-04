d67f032c2901ff7edb52aa796f19d1e3fc633b15
Miguel Godoy <miguelgodoyg@gmail.com>
2013-06-04 09:31:19 -0400

    Precompile Off

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,8 @@ TicTacToe::Application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  
+  config.assets.enabled = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
